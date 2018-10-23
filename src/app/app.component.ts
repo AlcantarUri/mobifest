50d889060cb39f327d23a080fa10164ae3de239e
JoseManuelGdlO <josemanuelgarciadelao@gmail.com>
2018-10-22 22:39:17 -0500

    antes de añadir camara

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { MobiliarioPage } from '../pages/mobiliario/mobiliario';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = MobiliarioPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
